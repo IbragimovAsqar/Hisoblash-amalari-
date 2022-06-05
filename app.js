246a597963c45abf229e7bfda6c0e1a122b86c14
Asqar <ibragimovasqar@gmail.com>
2022-06-05 22:40:02 +0500

    Hisoblash amallari

---
+++ app.js
@@ -0,0 +1,35 @@
+const numberMath = +prompt('misollar soninig kiriting!')
+
+function randomNum(min, max) {
+ 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+function randomMath() {
+  const num1 = randomNum(1, 99)
+  const num2 = randomNum(1, 99)
+  const amal = randomNum(1, 4)
+  switch (amal) {
+    case 1: {
+      return { text: `${num1} + ${num2} =?`, ans: num1 + num2 }
+    }
+    case 2: {
+      return { text: `${num1} - ${num2} =?`, ans: num1 - num2 }
+    }
+    case 3: {
+      return { text: `${num1} * ${num2} =?`, ans: num1 * num2 }
+    }
+    case 4: {
+      return { text: `${num1} / ${num2} =?`, ans: num1 / num2 }
+    }
+  }
+}
+for (i = 0; i < numberMath; i++) {
+  const { text, ans } = randomMath()
+  const userInput = +prompt(text + 'javobni kiriting')
+  if (userInput === ans) {
+    alert('Siznign javob togri')
+    continue
+  }
+  
+  alert('Sizning javob noto`g`ri! To`g`ir javob: ' + ans)
+}
